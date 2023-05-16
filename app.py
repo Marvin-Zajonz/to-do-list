4665fbc6275ac9bd167eb0537822c315d0822ea6
Marvin-Zajonz <zajonzmarvin@gmail.com>
2023-05-16 22:02:36 +0200

    Added initial routes to Flask application

--- app.py
+++ app.py
@@ -1,12 +1,68 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# import your database models here, e.g.
+# from models import db, User, Task
 
 app = Flask(__name__)
 
+# configure your app, e.g.
+# app.config['SECRET_KEY'] = 'your-secret-key'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'your-database-uri'
+# db.init_app(app)
+
+# login_manager = LoginManager()
+# login_manager.login_view = 'login'
+# login_manager.init_app(app)
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@login_required
+def home():
+    # Display the dashboard page with an overview of tasks
+    return render_template('dashboard.html')
 
+@app.route('/task', methods=['GET', 'POST'])
+@login_required
+def create_task():
+    # Handle task creation
+    return render_template('create_task.html')
+
+@app.route('/task/<int:task_id>', methods=['GET', 'POST'])
+@login_required
+def view_task(task_id):
+    # Handle task viewing and updating
+    return render_template('task_detail.html')
+
+@app.route('/task/<int:task_id>/delete', methods=['POST'])
+@login_required
+def delete_task(task_id):
+    # Handle task deletion
+    return redirect(url_for('home'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # Handle user registration
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Handle user login
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    # Handle user logout
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/settings', methods=['GET', 'POST'])
+@login_required
+def settings():
+    # Handle user settings
+    return render_template('settings.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
