a549c845a96f576f76c21fe6e21eba90a226f93b
Marvin-Zajonz <zajonzmarvin@gmail.com>
2023-05-19 17:45:39 +0200

    Added db.relationship in User class for one to many relationship

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+    tasks = db.relationship('Task', backref='user', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
